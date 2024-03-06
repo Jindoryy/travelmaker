23c5e216137cdfc1c8fcf92af83973388a8ecd97
이진규 <tyjk8997@gmail.com>
2024-03-06 12:27:44 +0900

    [Init] 메인서버 초기세팅

---
+++ server_main/src/main/java/com/a305/travelmaker/TravelmakerApplication.java
@@ -0,0 +1,13 @@
+package com.a305.travelmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelmakerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TravelmakerApplication.class, args);
+  }
+
+}
---
+++ server_main/src/main/java/com/a305/travelmaker/domain/user/domain/User.java
@@ -0,0 +1,22 @@
+package com.a305.travelmaker.domain.user.domain;
+
+import com.a305.travelmaker.global.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+}
