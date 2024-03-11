b42075cf709f8a478779cfba3e5634983d9abec2
이건희 <dlrjsgml109@gmail.com>
2024-03-11 09:15:10 +0900

    [Init] React 디렉토리 설정, util 함수 설정

---
+++ client/src/utils/theme.js
@@ -0,0 +1,20 @@
+const theme = {
+    // 이 코드는 예시
+    light: {
+        main: '#0B132B',
+        sub: '#D9E3E2',
+        maintext: '#0B132B',
+        subtext: '#D9E3E2',
+        point: '#E06A4E',
+    },
+    dark: {
+        main: '#0B132B',
+        sub: '#0B132B',
+        maintext: '#0B132B',
+        subtext: '#0B132B',
+        point: '#E06A4E',
+    }
+}
+
+
+export default theme;
