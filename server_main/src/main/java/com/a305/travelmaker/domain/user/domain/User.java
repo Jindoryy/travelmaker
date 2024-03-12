fce4bb1ccc769a373e11a38c6568264a07e3ee9d
이건희 <dlrjsgml109@gmail.com>
2024-03-12 17:19:38 +0900

    [Feat] User 객체 생성

--- server_main/src/main/java/com/a305/travelmaker/domain/user/domain/User.java
+++ server_main/src/main/java/com/a305/travelmaker/domain/user/domain/User.java
@@ -1,22 +1,43 @@
 package com.a305.travelmaker.domain.user.domain;
 
+import com.a305.travelmaker.domain.user.data.UserStatus;
 import com.a305.travelmaker.global.common.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+
 @Entity
+@Table(name = "USER")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
+
+    // 사용자 태그를 저장하는 필드
+    @Column(nullable = false)
+    private int tag;
+
+    // 사용자 닉네임을 저장하는 필드
+    @Column(nullable = false)
+    private String nickname;
+
+    // 사용자 이메일을 저장하는 필드
+    @Column(nullable = false)
+    private String email;
+
+    // 사용자 프로필 사진 URL을 저장하는 필드
+    @Column(length = 2000)
+    private String profileUrl;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserStatus status = UserStatus.BEFORE_COURSE;
 }
