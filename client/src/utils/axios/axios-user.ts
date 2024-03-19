cbf48e06b12e76da9a4d1c5c2aabebfb7ad72999
이건희 <dlrjsgml109@gmail.com>
2024-03-19 18:46:26 +0900

    [Feat] Zustand User정보, 인증토큰, 재발급 토큰 store 및 localstorage에 저장 - Zustand User파트 인터페이스 설정 - axios-user 인터페이스 설정 - 소셜 로그인 랜딩 페이지 누락 수정 - User 정보, 인증토큰, 재발급 토큰 store처리 - User 정보, 인증토큰, 재발급 토큰 localstorage처리 [Related to] S10P22A305-317

--- client/src/utils/axios/axios-user.ts
+++ client/src/utils/axios/axios-user.ts
@@ -2,8 +2,18 @@ import { AxiosResponse } from "axios";
 import instance from "./axios-instance";
 
 interface KakaoAuthResponse {
-    accessToken: string;
-    // 응답에 포함된 다른 필드들도 여기에 추가하세요.
+    status: string;
+    data: {
+      userId: number;
+      nickName: string;
+      profileUrl: string;
+      status: string;
+      tokenType: string;
+      accessToken: string;
+      expiresIn: number;
+      refreshToken: string;
+      refreshTokenExpiresIn: number;
+    };
 }
 
 const kakaoauthentication = (code: string): Promise<AxiosResponse<KakaoAuthResponse>> => {
