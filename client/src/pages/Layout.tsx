915ffc6654e07ca14041da3c31a6d79964274f51
박지원 <wldnjs11234@naver.com>
2024-03-19 15:45:18 +0900

    [Feat] 하단 내브바 구현 완료(기능연결필요)

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,13 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer';
+import styled from 'styled-components';
+import Box from '@mui/material/Box';
 
 const Layout = () => {
   return (
-    <>
+    <ContainerBox>
       <Outlet />
       <Footer />
-    </>
+    </ContainerBox>
   );
 };
 
+const ContainerBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 export default Layout;
