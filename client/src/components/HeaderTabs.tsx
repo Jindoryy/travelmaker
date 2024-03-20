0108c0b9dfc2a7e999faed0549704f20c15928e7
박지원 <wldnjs11234@naver.com>
2024-03-20 15:39:47 +0900

    [Refactor] 헤더버튼, 하단내브바, 전체적인 화면 width설정변경, 코스확정전페이지 css수정

--- client/src/components/HeaderTabs.tsx
+++ client/src/components/HeaderTabs.tsx
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 
 const HeaderTabs = ({ selectedTab, letters, onTabChange, size }: any) => {
   return (
     <HeaderBox>
       {Array.from({ length: size }, (_, index) => (
         <OneButton
-          disableRipple
           key={index}
           onClick={() => onTabChange(index + 1)}
           className={selectedTab === index + 1 ? 'active' : ''}
@@ -26,35 +24,25 @@ const HeaderBox = styled(Box)`
     flex-direction: row;
     align-items: center;
     justify-content: center;
-    margin: auto;
-    margin-top: 10px;
+    margin: 10px 0px;
     padding: 2px 4px;
     height: 50px;
     background-color: #f2f4f6;
     border-radius: 30px;
-    hover: none;
   }
 `;
 
-const OneButton = styled(Button)`
-  && {
-    display: flex;
-    width: 130px;
-    height: 40px;
-    border-radius: 30px;
-    background-color: ${(props) =>
-      props.className === 'active' ? 'rgb(86, 108, 240, 0.8)' : '#f2f4f6'};
-    color: ${(props) => (props.className === 'active' ? 'white' : '#B1AFAF')};
-    font-family: 'Pretendard';
-    font-size: 16px;
-    font-weight: bold;
-    . &:hover {
-      background-color: rgb(86, 108, 240, 0.8);
-    }
-    &:visited {
-      background-color: rgb(86, 108, 240, 0.8);
-    }
-  }
+const OneButton = styled.button`
+  width: 130px;
+  height: 40px;
+  border-radius: 30px;
+  background-color: ${(props) =>
+    props.className === 'active' ? 'rgba(86, 108, 240, 0.8)' : '#f2f4f6'};
+  color: ${(props) => (props.className === 'active' ? 'white' : '#B1AFAF')};
+  font-family: 'Pretendard';
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
 `;
 
 export default HeaderTabs;
