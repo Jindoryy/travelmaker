d7b01d0c8146f1a290b1ebafbdfc68e52d973b56
youngjin <poweryj1@naver.com>
2024-03-21 08:58:02 +0900

    [Feat] 좋아요 기반 cbf 추천 알고리즘 500개 출력

--- server_bigdata/recommend/views.py
+++ server_bigdata/recommend/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+import random
 import pandas as pd
 import numpy as np
 from collections import Counter
@@ -15,14 +16,29 @@ from surprise import Dataset, Reader, KNNBasic, SVD
 from surprise.model_selection import train_test_split
 from surprise.accuracy import rmse
 
-# from recommend.models import Destination,Like, DestinationCopy, Category, LikeCopy
-# from recommend.serializers import DestinationSerializer,LikeSerializer, DestinationCopySerializer, CategorySerializer, LikeCopySerializer
 from recommend.models import Destination,Likes
 from recommend.serializers import DestinationSerializer,LikeSerializer
 
-##
-# TF-IDF를 사용하여 특성 텍스트를 벡터화하고, 사용자의 특성을 기반으로 코사인 유사도를 계산하여 유사한 장소를 추천합니다.
 
+########################################## 좋아요 기반 추천 ##########################################
+@api_view(['POST'])
+def getTravelList(request):
+    # 요청의 본문에서 JSON 형식의 데이터 가져오기
+    data_from_body = request.data
+    print(data_from_body)
+    print("latitude 값:", data_from_body['center_coordinates']['latitude'])
+    print("location_ids 값:", data_from_body['location_ids'])
+
+    # 여기서 데이터 처리를 수행하고 원하는 응답을 반환
+    # 예를 들어, 받은 데이터를 가공하여 응답으로 반환
+    processed_data = {}  # 데이터 가공 예시 (실제로는 여기에서 데이터 처리를 수행해야 합니다.)
+
+    return Response(processed_data)
+
+
+########################################## 좋아요 기반 추천 ##########################################
+# TF-IDF를 사용하여 특성 텍스트를 벡터화하고, 사용자의 특성을 기반으로 코사인 유사도를 계산하여 유사한 장소를 추천합니다.
+# 그나마 비슷한 애들로 추천 + 목록 골고루 섞기
 @api_view(['GET'])
 def getLikeCbfList(request, user_id):
     # 사용자가 좋아요를 누른 장소의 특성을 가져오기
@@ -53,8 +69,8 @@ def getLikeCbfList(request, user_id):
     similar_destinations = [destination for destination in similar_destinations if destination.DESTINATION_ID not in user_liked_destination_ids]
 
     # 최대한 비슷한 장소로 30개를 채워주기
-    if len(similar_destinations) < 30:
-        remaining_recommendations = 30 - len(similar_destinations)
+    if len(similar_destinations) < 100:
+        remaining_recommendations = 100 - len(similar_destinations)
         # 비슷한 장소를 추가로 찾아서 추천 목록에 추가
         more_similar_destinations = Destination.objects.exclude(DESTINATION_ID__in=user_liked_destination_ids).exclude(pk__in=[d.pk for d in similar_destinations])[:remaining_recommendations]
         similar_destinations.extend(more_similar_destinations)
@@ -68,15 +84,23 @@ def getLikeCbfList(request, user_id):
     user_feature_text = ' '.join(user_features)
     user_tfidf = tfidf_vectorizer.transform([user_feature_text])
     similarities = cosine_similarity(user_tfidf, tfidf_matrix).flatten()
-
+    
     # 유사도에 따라 장소 정렬
     similar_destinations = [similar_destinations[i] for i in similarities.argsort()[::-1]]
 
-    # 유사한 장소들의 DESTINATION_ID만을 포함한 리스트로 변환
-    similar_destinations_ids = [destination.DESTINATION_ID for destination in similar_destinations]
+    # 목록을 무작위로 섞기
+    random.shuffle(similar_destinations)
 
-    return Response(similar_destinations_ids, status=status.HTTP_200_OK)
+    # 추천 결과에서 DESTINATION_ID만 추출하여 리스트에 담기
+    destination_ids = [destination.DESTINATION_ID for destination in similar_destinations]
 
+    # 500개의 장소로 제한
+    destination_ids = destination_ids[:500]
+
+    return Response(destination_ids, status=status.HTTP_200_OK)
+
+
+# 그나마 비슷한 애들로 추천 + 목록 골고루 섞기
 @api_view(['GET'])
 def getLikeCbfDetail(request, user_id):
     # 사용자가 좋아요를 누른 장소의 특성을 가져오기
@@ -107,8 +131,8 @@ def getLikeCbfDetail(request, user_id):
     similar_destinations = [destination for destination in similar_destinations if destination.DESTINATION_ID not in user_liked_destination_ids]
 
     # 최대한 비슷한 장소로 30개를 채워주기
-    if len(similar_destinations) < 30:
-        remaining_recommendations = 30 - len(similar_destinations)
+    if len(similar_destinations) < 100:
+        remaining_recommendations = 100 - len(similar_destinations)
         # 비슷한 장소를 추가로 찾아서 추천 목록에 추가
         more_similar_destinations = Destination.objects.exclude(DESTINATION_ID__in=user_liked_destination_ids).exclude(pk__in=[d.pk for d in similar_destinations])[:remaining_recommendations]
         similar_destinations.extend(more_similar_destinations)
@@ -122,19 +146,22 @@ def getLikeCbfDetail(request, user_id):
     user_feature_text = ' '.join(user_features)
     user_tfidf = tfidf_vectorizer.transform([user_feature_text])
     similarities = cosine_similarity(user_tfidf, tfidf_matrix).flatten()
-    print(similarities)
+    
     # 유사도에 따라 장소 정렬
     similar_destinations = [similar_destinations[i] for i in similarities.argsort()[::-1]]
 
-    # 추천 결과를 직렬화
-    # serializer = DestinationSerializer(similar_destinations[:30], many=True)
-    serializer = DestinationSerializer(similar_destinations, many=True)
+    # 목록을 무작위로 섞기
+    random.shuffle(similar_destinations)
+
+    # 추천 결과를 직렬화 (500개)
+    # serializer = DestinationSerializer(similar_destinations, many=True)
+    serializer = DestinationSerializer(similar_destinations[:500], many=True)
 
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 
-########################################## 더미데이터 #################################################
+########################################## 더미데이터 ##########################################
 
 @api_view(['GET'])
 def getLike(request, user_id):
@@ -192,81 +219,81 @@ def getDestination(request):
 
 ######################################## 원-핫 인코딩 ###################################
 ################################# cf:surprise 사용 ######################################
-@api_view(['GET'])
-def recommend_destinations(request, user_id):
-    try:
-        if not Like.objects.filter(USER_ID=user_id).exists():
-            return Response({"message": "사용자가 좋아하는 항목이 없습니다."})
+# @api_view(['GET'])
+# def recommend_destinations(request, user_id):
+#     try:
+#         if not Like.objects.filter(USER_ID=user_id).exists():
+#             return Response({"message": "사용자가 좋아하는 항목이 없습니다."})
 
-        like = Like.objects.filter(FLAG=True, USER_ID=user_id)
-        like_values = list(like.values_list('DESTINATION_ID', 'FEATURE'))
+#         like = Like.objects.filter(FLAG=True, USER_ID=user_id)
+#         like_values = list(like.values_list('DESTINATION_ID', 'FEATURE'))
 
-        all_features = [feature.strip() for _, features in like_values for feature in features.split(',')]
-        feature_counts = Counter(all_features)
-        top_3_features = [feature[0] for feature in feature_counts.most_common(3)]
+#         all_features = [feature.strip() for _, features in like_values for feature in features.split(',')]
+#         feature_counts = Counter(all_features)
+#         top_3_features = [feature[0] for feature in feature_counts.most_common(3)]
 
-        liked_items = [(item[0], item[1]) for item in like_values]
+#         liked_items = [(item[0], item[1]) for item in like_values]
 
-        destinations = Destination.objects.all()
-        destination_features = [destination.FEATURE.split(',') for destination in destinations]
+#         destinations = Destination.objects.all()
+#         destination_features = [destination.FEATURE.split(',') for destination in destinations]
 
-        destination_feature_array = []
-        destination_scores = {}
+#         destination_feature_array = []
+#         destination_scores = {}
 
-        for dest_features, destination in zip(destination_features, destinations):
-            dest_feature_vector = [1 if feature in dest_features else 0 for feature in top_3_features]
-            score = 0
-            for rank, feature in enumerate(top_3_features, start=1):
-                if feature in dest_features:
-                    score += (4 - rank)
-            destination_scores[destination] = score
-            destination_feature_array.append(dest_feature_vector)
+#         for dest_features, destination in zip(destination_features, destinations):
+#             dest_feature_vector = [1 if feature in dest_features else 0 for feature in top_3_features]
+#             score = 0
+#             for rank, feature in enumerate(top_3_features, start=1):
+#                 if feature in dest_features:
+#                     score += (4 - rank)
+#             destination_scores[destination] = score
+#             destination_feature_array.append(dest_feature_vector)
 
-        destination_feature_array = np.array(destination_feature_array)
+#         destination_feature_array = np.array(destination_feature_array)
 
-        # 모든 가능한 조합 생성
-        feature_combinations = []
-        for r in range(len(top_3_features), 0, -1):
-            feature_combinations += list(combinations(top_3_features, r))
+#         # 모든 가능한 조합 생성
+#         feature_combinations = []
+#         for r in range(len(top_3_features), 0, -1):
+#             feature_combinations += list(combinations(top_3_features, r))
 
-        # 사용자가 선호하는 상위 특성에 따라 목적지 필터링
-        filtered_destinations = []
-        for dest, features in zip(destinations, destination_features):
-            for combination in feature_combinations:
-                if all(feature in features for feature in combination):
-                    filtered_destinations.append(dest)
-                    break
+#         # 사용자가 선호하는 상위 특성에 따라 목적지 필터링
+#         filtered_destinations = []
+#         for dest, features in zip(destinations, destination_features):
+#             for combination in feature_combinations:
+#                 if all(feature in features for feature in combination):
+#                     filtered_destinations.append(dest)
+#                     break
 
-        # 사용자-목적지 평가 행렬 구축
-        reader = Reader(rating_scale=(1, 5))
-        data = [(user_id, dest_id, 5) for dest_id, _ in liked_items]
-        dataset = Dataset.load_from_df(pd.DataFrame(data, columns=['user_id', 'item_id', 'rating']), reader)
+#         # 사용자-목적지 평가 행렬 구축
+#         reader = Reader(rating_scale=(1, 5))
+#         data = [(user_id, dest_id, 5) for dest_id, _ in liked_items]
+#         dataset = Dataset.load_from_df(pd.DataFrame(data, columns=['user_id', 'item_id', 'rating']), reader)
         
-        trainset = dataset.build_full_trainset()
+#         trainset = dataset.build_full_trainset()
 
-        # KNNBasic 모델 학습
-        sim_options = {
-            'name': 'cosine',
-            'user_based': False
-        }
-        algo = KNNBasic(sim_options=sim_options)
-        algo.fit(trainset)
+#         # KNNBasic 모델 학습
+#         sim_options = {
+#             'name': 'cosine',
+#             'user_based': False
+#         }
+#         algo = KNNBasic(sim_options=sim_options)
+#         algo.fit(trainset)
 
-        # 추천 목록 생성
-        recommendations = []
-        for dest in filtered_destinations:
-            if dest.DESTINATION_ID not in [item[0] for item in liked_items]:
-                prediction = algo.predict(user_id, dest.DESTINATION_ID)
-                recommendations.append((dest, prediction.est))
+#         # 추천 목록 생성
+#         recommendations = []
+#         for dest in filtered_destinations:
+#             if dest.DESTINATION_ID not in [item[0] for item in liked_items]:
+#                 prediction = algo.predict(user_id, dest.DESTINATION_ID)
+#                 recommendations.append((dest, prediction.est))
 
-        # 점수에 따라 내림차순 정렬
-        recommendations.sort(key=lambda x: x[1], reverse=True)
+#         # 점수에 따라 내림차순 정렬
+#         recommendations.sort(key=lambda x: x[1], reverse=True)
 
-        # 최종 추천 목록에 추가
-        recommended_destinations = [dest for dest, _ in recommendations[:20]]
+#         # 최종 추천 목록에 추가
+#         recommended_destinations = [dest for dest, _ in recommendations[:20]]
 
-        serializer = DestinationSerializer(recommended_destinations, many=True)
-        return Response(serializer.data)
+#         serializer = DestinationSerializer(recommended_destinations, many=True)
+#         return Response(serializer.data)
     
-    except Like.DoesNotExist:
-        return Response({"message": "사용자가 좋아하는 항목이 없습니다."})
+#     except Like.DoesNotExist:
+#         return Response({"message": "사용자가 좋아하는 항목이 없습니다."})
