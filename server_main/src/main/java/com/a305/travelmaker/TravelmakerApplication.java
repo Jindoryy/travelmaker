e4a0ba1b7566f17c097708a0987a0f4b7ea96668
이건희 <dlrjsgml109@gmail.com>
2024-03-22 03:33:45 +0900

    [Feat] Redis 초기 설정 - 메인 클래스 Redis 어노테이션 적용 - implements 추가

--- server_main/src/main/java/com/a305/travelmaker/TravelmakerApplication.java
+++ server_main/src/main/java/com/a305/travelmaker/TravelmakerApplication.java
@@ -3,8 +3,10 @@ package com.a305.travelmaker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @EnableJpaAuditing
+@EnableRedisRepositories
 @SpringBootApplication
 public class TravelmakerApplication {
 
