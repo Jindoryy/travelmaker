43ade7415645fc8d6d99299da5109fd8a9f64c97
이건희 <dlrjsgml109@gmail.com>
2024-03-21 18:05:59 +0900

    [Feat] 도 선택 페이지 구현 - ProvincePage 구현 - ProvinceList 구현 - ProvinceButton 구현 - axios 코드 및 응답 인터페이스 설정 - 로딩 컴포넌트 설정 [Related to] S10P22A305-349

---
+++ client/src/features/course/ProvinceButton.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+interface ImageProps {
+  content: {
+    provinceId: number;
+    provinceName: string;
+    provinceUrl: string;
+  };
+}
+
+const ProvinceButton: React.FC<ImageProps> = ({ content }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/course/city', { state: { provinceId: content.provinceId } });
+  };
+
+  return (
+    <Container onClick={handleClick}>
+      <Image src={content.provinceUrl} alt={content.provinceName} />
+      <TextOverlay>{content.provinceName}</TextOverlay>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 10rem;
+  height: 10rem;
+  position: relative;
+`;
+
+const Image = styled.img`
+  width: 10rem;
+  height: 10rem;
+  left: 0;
+  top: 0;
+  position: absolute;
+  border-radius: 0.5rem;
+`;
+
+const TextOverlay = styled.div`
+  width: 10rem;
+  height: 2.5rem;
+  left: 0;
+  top: 6.675rem;
+  position: absolute;
+  text-align: center;
+  color: #ffc65c;
+  font-family: 'Black Han Sans', sans-serif;
+  font-weight: normal;
+  font-size: 2.8rem;
+`;
+
+export default ProvinceButton;
