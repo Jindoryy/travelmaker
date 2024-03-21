6065261935cf049d93a87718a45f81a584bfdd94
yeonhwa <itidnsld@naver.com>
2024-03-21 15:06:54 +0900

    [Feat] 헤더 스크롤 애니메이션 적용

--- client/src/components/MainProfile.tsx
+++ client/src/components/MainProfile.tsx
@@ -1,7 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
+interface ProfileProps {
+  scrolled: boolean;
+  scrollHeight: number;
+  fontSize: string;
+}
+
+interface ProfileImageProps {
+  size: string;
+}
+
 const Profile = () => {
+  const [scrolled, setScrolled] = useState(false);
+  const [scrollHeight, setScrollHeight] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollHeight = window.scrollY;
+      setScrollHeight(currentScrollHeight);
+
+      if (currentScrollHeight > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   const [userState, setUserState] = useState<'beforeCourse' | 'beforeTravel' | 'onTravel'>(
     'beforeCourse',
   );
@@ -38,33 +70,88 @@ const Profile = () => {
     }
   };
 
+  const getProfileSize = () => {
+    if (scrollHeight > 200) {
+      return '100px';
+    } else if (scrollHeight > 100) {
+      return '150px';
+    } else if (scrollHeight > 50) {
+      return '200px';
+    } else {
+      return '250px';
+    }
+  };
+
+  const getProfileContentFontSize = () => {
+    if (scrollHeight > 200) {
+      return '16px';
+    } else if (scrollHeight > 100) {
+      return '18px';
+    } else if (scrollHeight > 50) {
+      return '20px';
+    } else {
+      return '22px';
+    }
+  };
+
   return (
-    <StyledProfileContainer>
-      <StyledProfileImage src={getProfileImage()} alt="Profile" />
-      <StyledProfileContent>{getProfileContent()}</StyledProfileContent>
+    <StyledProfileContainer
+      scrolled={scrolled}
+      scrollHeight={scrollHeight}
+      fontSize={getProfileContentFontSize()}
+    >
+      <StyledProfileImage src={getProfileImage()} alt="Profile" size={getProfileSize()} />
+      <StyledProfileContent
+        scrolled={scrolled}
+        scrollHeight={scrollHeight}
+        fontSize={getProfileContentFontSize()}
+      >
+        {getProfileContent()}
+      </StyledProfileContent>
       <br />
     </StyledProfileContainer>
   );
 };
 
-const StyledProfileContainer = styled.div`
+const StyledProfileContainer = styled.div<ProfileProps>`
   background-color: white;
   color: #000;
   padding: 10px;
+  padding-bottom: 15px;
   margin: 10px;
   text-align: center;
+  align-items: center;
   border-radius: 15px;
+  transition: height 0.1s;
+  height: ${({ scrolled, scrollHeight }) => {
+    if (scrolled && scrollHeight >= 1000) {
+      return '120px';
+    } else {
+      if (scrollHeight > 200) {
+        return '120px';
+      } else if (scrollHeight > 100) {
+        return '200px';
+      } else if (scrollHeight > 50) {
+        return '250px';
+      } else {
+        return '300px';
+      }
+    }
+  }};
 `;
 
-const StyledProfileContent = styled.p`
-  font-size: 20px;
+const StyledProfileContent = styled.p<ProfileProps>`
+  font-size: ${({ fontSize }) => fontSize};
   font-family: 'Pretendard';
   font-weight: bold;
+  transition: font-size 0.1s;
 `;
 
-const StyledProfileImage = styled.img`
+const StyledProfileImage = styled.img<ProfileImageProps>`
   max-width: 100%;
   height: auto;
+  transition: height 0.1s;
+  height: ${({ size }) => size};
 `;
 
 export default Profile;
