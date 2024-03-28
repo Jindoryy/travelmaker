b922c556a169c64d472aa390c13bbd3fd08026da
전성재 <jeonseonjae@naver.com>
2024-03-28 17:56:28 +0900

    [Feat] ã스케줄링 구현

--- server_main/src/main/java/com/a305/travelmaker/domain/user/entity/User.java
+++ server_main/src/main/java/com/a305/travelmaker/domain/user/entity/User.java
@@ -13,6 +13,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @Entity
@@ -21,6 +22,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
+@Setter
 public class User extends BaseEntity {
 
     @Id
