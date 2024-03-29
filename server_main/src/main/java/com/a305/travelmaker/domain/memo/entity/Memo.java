d7567a226539f485eb9a23ecf2dd49ca3b3c7a20
전성재 <jeonseonjae@naver.com>
2024-03-29 15:53:02 +0900

    [Feat] 메모 api 생성
    -
    
     스케쥴링 구현
    
    [Resolves] S10P22A305-484
    [Ref]
    [Related to]

--- server_main/src/main/java/com/a305/travelmaker/domain/memo/entity/Memo.java
+++ server_main/src/main/java/com/a305/travelmaker/domain/memo/entity/Memo.java
@@ -11,11 +11,16 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Memo extends BaseEntity {
 
