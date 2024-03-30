1c473520433ba5f7b29e706970c6dd6ee5837e01
이건희 <dlrjsgml109@gmail.com>
2024-03-30 21:23:51 +0900

    [Fix] userStore 수정

--- client/src/store/useUserStore.ts
+++ client/src/store/useUserStore.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 interface UserInfo {
     userId: number;
@@ -19,14 +19,31 @@ const useUserInfo  = create(
     persist<UserInfoState>(
     (set) => ({
         userInfo: initialState,
-        setUserInfo: (userInfo) => set({ userInfo }),
-        clearUserInfo: () => set({ userInfo: initialState}),
+        // 사용자 정보 설정 함수
+        setUserInfo: (userInfo) => {
+            console.log("setUserInfo 호출 전 상태:", userInfo);
+            set({ userInfo });
+            console.log("setUserInfo 호출 후 상태:", userInfo);
+        },
+        // 사용자 정보 초기화 함수
+        clearUserInfo: () => {
+            set({ userInfo: initialState });
+        },
     }),
     {
         name: "userInfo",
-        getStorage: () => localStorage,
+        storage: createJSONStorage(() => localStorage),
+        version: 1.0,
+        // persist 옵션에 onRehydrateStorage 추가하여 상태 복원 시 로그 추가
+        onRehydrateStorage: () => (state, error) => {
+            if (error) {
+                console.log("상태 복원 중 오류 발생:", error);
+            }
+            console.log("상태 복원 후:", state);
+        },
     }
     )
-)
+);
+
 
 export default useUserInfo;
