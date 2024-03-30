f784f129443ee92250a5930d0e66e177093fff9b
이건희 <dlrjsgml109@gmail.com>
2024-03-30 12:04:02 +0900

    [Feat] 유저 정보 반환 로직 추가 - 유저 여행 상태 여부(Status) 반환 - 유저 성별-생년월일 입력 여부(boolean) 반환 - 해당 유저 미작성 일기 있는지 여부(boolean) 반환 - 미작성 일기 조회 service, repository 추가 - UserService 에러 처리 리팩토링 - UserController 반환값 리팩토링, 유저 인증 과정 추가 - 테스트용 더미 UserController 추가 [Related to] S10P22A305-475

--- server_main/src/main/java/com/a305/travelmaker/domain/travel/repository/TravelRepository.java
+++ server_main/src/main/java/com/a305/travelmaker/domain/travel/repository/TravelRepository.java
@@ -2,13 +2,16 @@
 
 package com.a305.travelmaker.domain.travel.repository;
 
+import com.a305.travelmaker.domain.travel.dto.DiaryStatus;
 import com.a305.travelmaker.domain.travel.entity.Travel;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TravelRepository extends JpaRepository<Travel, Integer> {
 
   List<Travel> findByUserId(Long userId);
-
+  long countByUserIdAndStatusAndStartDateBetween(Long userId, DiaryStatus status, LocalDate start, LocalDate end);
+  List<Travel> findByUserIdAndStatusAndStartDateBetween(Long userId, DiaryStatus status, LocalDate start, LocalDate end);
 }
--- server_main/src/main/java/com/a305/travelmaker/domain/user/dto/UserStatusResponse.java
+++ server_main/src/main/java/com/a305/travelmaker/domain/user/dto/UserStatusResponse.java
@@ -1,18 +1,17 @@
-// UserStatusResponse.java
-
 package com.a305.travelmaker.domain.user.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
+@Builder
 @AllArgsConstructor
 public class UserStatusResponse {
-    private String status;
 
-    public UserStatusResponse(UserStatus status) {
-        this.status = status.toString();
-    }
+    private String status;
+    private Boolean birthCheck;// 생년월일 미 입력시(null일시) false
+    private Boolean genderCheck;// 성별 미 입력시(null일시) false
+    private Boolean diaryCheck;// 작성해야 할 여행 일기가 있으면 true 없으면 false
 }
+
