9fda2e077bce3f5798e59b76be5b0e96512f4369
이건희 <dlrjsgml109@gmail.com>
2024-04-01 00:01:37 +0900

    [Feat] 마이 페이지 구현 - 일정 일기 구분 탭 - 일정 리스트 - 일기 리스트 - 일정 컴포넌트 삭제 버튼 - 일정 컴포넌트 상황별 조건부 버튼 및 navigate설정 - 일기 컴포넌트 Grid 설정 및 navigate 설정 - 해당 컴포넌트 기능별 axios 정리 [Related to] S10P22A305-475

---
+++ client/src/features/user/ScheduleStatusButton .tsx
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+const ScheduleStatusButton: React.FC<StatusProps> = ({ data }) => {
+  const { travelId, status } = data;
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    switch (status) {
+      case '코스보기':
+        navigate(`/course/detail/${travelId}`);
+        break;
+      case '일기작성':
+        navigate(`/diary/write/${travelId}`);
+        break;
+      case '일기보기':
+        navigate(`/diary/detail/${travelId}`);
+        break;
+      default:
+        console.error('상태값 props 에러, 체크 필요');
+    }
+  };
+
+  return (
+    <Button onClick={handleClick} status={status}>
+      {status}
+    </Button>
+  );
+};
+
+export default ScheduleStatusButton;
+
+interface StatusData {
+  travelId: number;
+  status: '코스보기' | '일기작성' | '일기보기';
+}
+interface StatusProps {
+  data: StatusData;
+}
+
+interface ButtonProps {
+  status: '코스보기' | '일기작성' | '일기보기';
+}
+
+const Button = styled.div<ButtonProps>`
+  width: 80px;
+  height: 30px;
+  border-radius: 15px;
+  color: white;
+  background-color: ${({ status }) => {
+    switch (status) {
+      case '코스보기':
+        return '#FFE198';
+      case '일기작성':
+        return '#FFC6A4';
+      case '일기보기':
+        return '#FF65A3';
+      default:
+        return 'transparent';
+    }
+  }};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 14px;
+  font-weight: bold;
+  cursor: pointer;
+  user-select: none;
+`;
