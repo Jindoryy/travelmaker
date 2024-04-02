6f4c6514ea0164910a0d4ff7d024c81488845879
박지원 <wldnjs11234@naver.com>
2024-04-03 08:55:00 +0900

    [Feat] 글씨체적용, checksite변경

--- client/src/pages/course/DateTransChoice.tsx
+++ client/src/pages/course/DateTransChoice.tsx
@@ -20,15 +20,14 @@ const DateTransChoice = () => {
   const [startDate, setStartDate] = useState<Dayjs | null>(null);
   const [endDate, setEndDate] = useState<Dayjs | null>(null);
   const [savedDate, setSavedDate] = useState<any>([]);
+  const [disableDate, setDisableDate] = useState<any>([]);
   const travelSaveStore = useTravelSave();
   const travelInfoStore = useTravelInfo();
   const userInfo = useUserInfo();
 
   useEffect(() => {
-    console.log(userInfo.userInfo.userId);
     getAlreadyConfirm(userInfo.userInfo.userId)
       .then((response) => {
-        console.log(response.data);
         const dates = response.data;
         if (dates) {
           const getSavedDate: string[] = [];
@@ -36,16 +35,14 @@ const DateTransChoice = () => {
             const startDate = new Date(dateObj.startDate);
             const endDate = new Date(dateObj.endDate);
             const currentDate = new Date(startDate);
-        
+
             while (currentDate <= endDate) {
               getSavedDate.push(currentDate.toISOString().slice(0, 10));
               currentDate.setDate(currentDate.getDate() + 1);
             }
           });
-          console.log(getSavedDate);
           setSavedDate(getSavedDate);
         }
-        
       })
       .catch((err) => {
         console.error(err);
@@ -107,8 +104,6 @@ const DateTransChoice = () => {
           transportation: selectedTransportations[0].alt,
           destinationIdList: [],
         });
-        console.log(travelSaveStore.travel);
-        console.log(travelInfoStore.travelInfo);
         navigate('/course/alonetogether');
       } else {
         Swal.fire({
@@ -169,6 +164,11 @@ const DateTransChoice = () => {
                   maxDate={
                     startDate ? startDate.add(2, 'day') : dayjs().startOf('day').add(2, 'day')
                   }
+                  shouldDisableDate={(date) => {
+                    const nextDay = dayjs(startDate).add(1, 'day').format('YYYY-MM-DD');
+                    const nextNextDay = dayjs(startDate).add(2, 'day').format('YYYY-MM-DD');
+                    return savedDate.includes(nextDay) || savedDate.includes(nextNextDay);
+                  }}
                 />
               </DatePickerContainer>
             </LocalizationProvider>
